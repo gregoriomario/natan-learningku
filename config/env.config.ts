9ad45235f70c719bac812ef8e5196c0a540a6071
unknown <mistzero0@gmail.com>
2022-06-13 12:58:07 +0800

    add certificate

--- config/env.config.ts
+++ config/env.config.ts
@@ -10,7 +10,7 @@ const env = {
 	URL: process.env.URL || "",
 	DB_USER: process.env.DB_USER || "admin",
 	DB_PASSWORD: process.env.DB_PASSWORD || "admin",
-	DB_PORT: process.env.DB_PORT || 5432,
+	DB_PORT: process.env.DB_PORT || 5433,
 	DB_HOST: process.env.DB_HOST || "localhost",
 	DB_NAME: process.env.DB_NAME || "",
 	DB_DRIVER: process.env.DB_DRIVER || "postgres",
