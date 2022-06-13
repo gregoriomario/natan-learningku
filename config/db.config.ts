5d9735ada64bdb23407ef1c183f9a1a8d36ab1d8
unknown <mistzero0@gmail.com>
2022-06-13 13:09:03 +0800

    add pg type

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,9 +1,11 @@
+import * as pg from "pg";
 import { Sequelize } from "sequelize";
 
 import ENV from "./env.config";
 
 const sequelize = new Sequelize(ENV.getDbString(), {
 	logging: false,
+	dialectModule: pg,
 });
 
 (async function () {
